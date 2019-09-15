21657ddb87076c10d4e7646a2c46a6e4ee2d3455
5M4CK3R-H4CK3R <55347225+5M4CK3R-H4CK3R@users.noreply.github.com>
2019-09-15 17:02:10 +0530

    Add files via upload

---
+++ maiis.py
@@ -0,0 +1,9 @@
+import os
+import time
+os.system('termux-tts-speak pleas wait starting maiis')
+os.chdir('/sdcard/111...Maiis/main')
+os.system('7z x maiis.7z -p@5M4CK3R#9817747175$')
+#os.system('@5M4CK3R#9817747175$')
+os.chdir('/sdcard/111...Maiis/main')
+os.system('python maiis1.py')
+# os.system('clear'@5M4CK3R#9817747175$)
